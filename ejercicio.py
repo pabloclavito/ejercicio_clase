5be7c8cecc0063342c2d969c2b69e47c6a9c68ab
Pablo MOrán <pmorangil7@gmail.com>
2023-10-10 19:39:36 +0200

    añado otra clase hija

--- ejercicio.py
+++ ejercicio.py
@@ -1,5 +1,12 @@
 #este es nuestro proyecto de python
 class Persona:
-	def __init__(self,nombre):
+	def __init__(self, nombre):
 		self.nombre = nombre
 
+class Bombero(Persona):
+	def __init__(self, nombre, gustos):
+		super().__init__(nombre)
+		self.gustos = gustos
+
+	def dialogo(self):
+		print("Soy un bombero")
